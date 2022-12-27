283b4437753be2a3ab497fe90f69e593c81803da
yaiir-a <42359972+yaiir-a@users.noreply.github.com>
2022-12-27 18:38:39 +0000

    try logging  URL

--- pages/api/all-users.js
+++ pages/api/all-users.js
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
+  console.log(process.env.DATABASE_URL);
   const allUsers = await prisma.users.findMany();
 
   res.status(200).json(allUsers);
